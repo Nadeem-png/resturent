da34017243fdc2e19e71641455a3e3d43df17b67
Nadeem khan <nk27890@gmail.com>
2021-05-25 11:36:07 -0700

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,31 @@
 @section('content')
 <style>
     .header{
-        background-image:linear-gradient(to right,#3204fdba,#9907facc), url({{asset('img/dish.png')}}) ;
+        background-image:linear-gradient(to right,#3204fdba,#9907facc), url({{asset('img/a3.jpg')}}) ;
         background-repeat: no-repeat;
         background-size: cover;
         position:center;
         margin:0;
-        
+
         height:560px;
         width: 100%;
     }
-    
-    
+
+.mid{
+    background-color: #fafafa;
+    height: 400px;
+}
+    .div4{
+        background-image: url({{asset('img/a2.jpg')}}) ;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position:center;
+        margin:0;
+
+        height:1200px;
+        width: 100%;
+    }
+
 </style>
 <div class="header d-flex justify-content-center align-items-center text-white flex-column">
 
@@ -24,16 +38,134 @@
    <button class="btn btn-outline btn-outline-danger mr-3 px-4 py-2">Place Order</button>
     <button class="btn btn-warning px-4 py-2 ">See More</button></div>
 </div>
-<div class="container mt-5 mx-5">
-<div class="row">
-<div class="col-sm-12 col-6">
-<h5></h5>
+<div class="container mt-5 mx-5 p-3">
+<div class="row d-flex justify-content-center align-items-center mx-lg-5 mx-md-5">
+<div class="col-lg-6 col-sm-">
+<h5 style="border-bottom: 2px solid yellow;width: 20%">Best Food</h5>
+
+    <h1>Welcome To Royal</h1>
+    <p>
+        We’ve been getting sick of using the same boring Latin text<br>
+        so we’ve found some really cool Lorem Ipsum Text Generators.<br>
+        I hope you can enjoy these as much as we do.<br>
+         We’ve been getting sick of using the same boring Latin text<br>
+        so we’ve found some really cool Lorem Ipsum Text Generators.<br>
+        I hope you can enjoy these as much as we do.
+    </p>
 </div>
-<div class="col-sm-12 col-6">
-<img class="img-fluid" src="{{asset('img/')}}">
+<div class="col-lg-6 ">
+<img class="img-fluid mb-5 " style="height: 500px; width:80%" src="{{asset('img/a11.jpg')}}">
 </div>
 </div>
 </div>
+    <div class="mid  d-flex justify-content-center align-items-center flex-column">
+        <h6 style="border-bottom: 2px solid orange ">Easy your Food</h6>
+        <h1 class="mb-3">Fresh Ingredient, Tastey Meals</h1>
+        <p align="center">   We’ve been getting sick of using the same boring Latin text
+            so we’ve<br> found some really cool Lorem Ipsum Text Generators.
+            I hope<br> you can enjoy these as much as we do.</p>
+        <button class="btn btn-primary px-4 py-2 ">View More</button>
+    </div>
+    <div class="div4">
+<div class="container-lg container-sm container-md">
+    <div class="row">
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white border border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+              <h5 class="text-warning">$450</h5>
+                <h3>Grill Fish</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-dark border text-white border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$40</h5>
+                <h3>Mushroom matar</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white border border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$50</h5>
+                <h3>Mushroom pyaza</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white border border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$40</h5>
+                <h3>Hamburger</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-dark border text-white border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$20</h5>
+                <h3>Salad</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$10 </h5>
+                <h3>Pasta</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white border border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$100</h5>
+                <h3>Rice.</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-dark border text-white border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$300</h5>
+                <h3> Malai Kofta</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="bg-white border border-dark-50 justify-content-center d-flex align-items-center flex-column"
+                 style="width: 250px;height: 250px">
+                <h5 class="text-warning">$450</h5>
+                <h3>Chole</h3>
+                <p align="center">   We’ve been getting sick of using the same boring Latin text
+                    so we’ve.</p>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
 
 @endsection
 @section('title','Home')
